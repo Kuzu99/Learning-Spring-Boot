9cca5bd12f018c66927f49890fee37a350acf744
Kuzu99 <kik3tkm@gmail.com>
2022-08-03 21:58:02 +0900

    #3 POST値を表示する

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,5 +19,16 @@ public class HelloController {
 	public String greeting(@RequestParam(defaultValue = "Hi!") String message, Model model) {
 		model.addAttribute("sample", message);
 		return "hello";
+	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "form";
+	}
+	
+	@PostMapping("/confirm")
+	public String confirm(@RequestParam String message, Model model) {
+		model.addAttribute("message", message);
+		return "confirm";
 	}
 }
